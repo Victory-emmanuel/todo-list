ba572fac7225e3715f8d2314da4d0d0001b9af8f
Victory Emmanuel <115421155+Victory-emmanuel@users.noreply.github.com>
2024-01-07 14:25:56 +0100

    Update TodoForm.js

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import styled from "styled-components";
 function TodoForm({ addTodo }) {
   //   Props Usage:
   // Receiving Props:
@@ -21,7 +22,7 @@ function TodoForm({ addTodo }) {
   // Calls the addTodo function (received as a prop) with the current value state, adding a new todo.
   // Resets the value state to an empty string after adding the todo.
   return (
-    <form id="todo-form" onSubmit={handleSubmit}>
+    <SearchForm id="todo-form" onSubmit={handleSubmit}>
       {/* The onSubmit event of the form is set to the handleSubmit function. */}
       <input
         type="text"
@@ -32,8 +33,8 @@ function TodoForm({ addTodo }) {
       {/* Input Field: Controlled Input: The input field's value is controlled by
       the value state. onChange event updates the value state whenever the input
       field's content changes (onChange={(e) => setValue(e.target.value)}). */}
-      <button type="submit">Add Task</button>
-    </form>
+      <Button type="submit">Add Task</Button>
+    </SearchForm>
   );
 }
 
@@ -43,6 +44,26 @@ function TodoForm({ addTodo }) {
 // Both components use the useState hook to manage their respective state (value in TodoForm and todos in TodoWrapper).
 // Props are used to pass down the addTodo function from the parent (TodoWrapper) to the child (TodoForm),
 //  enabling communication between the two components.
+
+const SearchForm = styled.form`
+  display: grid;
+  grid-template-columns: 78% 20%;
+  align-items: center;
+  padding: 0.75rem 1rem;
+  cursor: pointer;
+  width: 100%;
+  margin-top: 1rem;
+  grid-gap: 2%;
+  @media (max-width: 600px) {
+    grid-template-columns: 1fr;
+    justify-content: center;
+    grid-gap: 1rem;
+    // Adjust styles for smaller screens (sm)
+  }
+`;
+const Button = styled.button`
+  padding: 15px 5px;
+`;
 export default TodoForm;
 
 // TodoForm.jsx is a React component that renders a form for adding new todo items.It utilizes the useState hook to manage the state of the input field value.
