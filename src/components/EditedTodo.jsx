9f33910593389e6cce052ac6633299a17dfd5bfa
Victory Emmanuel <115421155+Victory-emmanuel@users.noreply.github.com>
2024-01-07 14:28:47 +0100

    Update EditedTodo.js

--- src/components/EditedTodo.jsx
+++ src/components/EditedTodo.jsx
@@ -1,19 +1,37 @@
 import React from "react";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
+import { Box, Typography } from "@mui/material";
 export const EditedTodo = ({ task, toggleComplete, deleteTodo, editTodo }) => {
   return (
-    <div key={task.id} id="added-todo">
-      <p
+    <Box
+      sx={{
+        display: "grid",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: "0.2rem 0.2rem",
+        borderRadius: "5px",
+        cursor: "pointer",
+        marginBottom: "1rem",
+        gridTemplateColumns: " 80% 20%",
+        background: " #ff6969",
+        width: "100%",
+      }}
+      key={task.id}
+      id="added-todo"
+    >
+      <Typography
+        variant="p"
+        component="p"
         onClick={() => toggleComplete(task.id)}
         className={`${task.completed ? "completed" : ""}`}
       >
         {task.task}
-      </p>
-      <div>
+      </Typography>
+      <Box>
         <EditIcon onClick={() => editTodo(task.id)} />
         <DeleteOutlinedIcon onClick={() => deleteTodo(task.id)} />
-      </div>
-    </div>
+      </Box>
+    </Box>
   );
 };
